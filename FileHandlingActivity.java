c80eb2f67d99c887211bdb577cb2bc81ca895741
Talia <thannani1@hwemail.com>
2025-09-15 13:02:56 -0700

    (FMC-0.4.2.0): create new java file
    Adds a new Java file called FileHandlingActivity.java
    
    This file will serve as the basis for implementing
    functionalities such as creating directories, writing
    to files, reading file contents, and creating backups.

---
+++ FileHandlingActivity.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class FileHandlingActivity {
+    public static void main(String[] args) {
+        // Your code here
+        
+        // a. Create main directory
+
+        // b. Create three text files
+        
+        // c. Write messages to files
+        
+        // d. Read and display file contents
+        
+        // e. Create backup directory
+        
+        // f. Copy contents to backup file
+        
+        // g. List all files in both directories
+    }
+}
