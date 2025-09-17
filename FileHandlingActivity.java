6d09794664ddb0f2dd244e4536ee429dbb48e11b
Talia <thannani1@hwemail.com>
2025-09-16 20:29:41 -0700

    (FMC-0.4.2.5) Implemented a file that reads and displays file contents
    program now prints the contents of notes.txt, data.txt, and log.txt to the console

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -4,11 +4,10 @@ import java.io.*;
 public class FileHandlingActivity {
     public static void main(String[] args) throws IOException {
         // Your code here
-        
+
         // a. Create main directory
         File directory = new File("JavaFileSystem");
         directory.mkdir();
-
 
         // b. Create three text files
         File note = new File(directory, "notes.txt");
@@ -20,28 +19,44 @@ public class FileHandlingActivity {
         File log = new File(directory, "log.txt");
         log.createNewFile();
 
-
         // c. Write messages to files
 
-            FileWriter writer = new FileWriter(note);
-            writer.write("Here is my special note.");
-            writer.close();
-            
-            FileWriter writer2 = new FileWriter(data);
-            writer2.write("Here is my second special note");
-            writer2.close(); 
+        FileWriter writer = new FileWriter(note);
+        writer.write("Here is my special note.");
+        writer.close();
 
-            FileWriter writer3 = new FileWriter(log);
-            writer3.write("Here is my third special note");
-            writer3.close(); 
+        FileWriter writer2 = new FileWriter(data);
+        writer2.write("Here is my second special note");
+        writer2.close();
 
-        
+        FileWriter writer3 = new FileWriter(log);
+        writer3.write("Here is my third special note");
+        writer3.close();
+
         // d. Read and display file contents
-        
+        readContent(note);
+        readContent(data);
+        readContent(log);
+
         // e. Create backup directory
-        
+
         // f. Copy contents to backup file
-        
+
         // g. List all files in both directories
     }
+    
+    public static void readContent(File file) throws FileNotFoundException, IOException {
+        try (FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr)) {
+            System.out.println("Contents of " + file.getName());
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+    }
+    
+
+
 }
